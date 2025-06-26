9e09c84f8a2291c186b008d1599635e102235bcc
devkitae <devkitae@gmail.com>
2025-06-26 15:42:26 +0900

    AccessInfo 화면 구성 - accessInfo.html 추가 - thymeleaf 적용을 위한 Controller 변경 - AccessInfo 정보 화면 단에 전달 - 기타 화면 정리

--- src/main/java/ktds/cloud/cloudkvtest/controller/AccessInfo.java
+++ src/main/java/ktds/cloud/cloudkvtest/controller/AccessInfo.java
@@ -1,36 +1,34 @@
 package ktds.cloud.cloudkvtest.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
-import ktds.cloud.cloudkvtest.entity.AccessInfo;
 import ktds.cloud.cloudkvtest.service.CheckAccessInfoService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created By Cheetah on 2025-06-18.
  */
-@RestController
+@Controller
 @RequiredArgsConstructor
-public class CheckAccessInfo {
+public class AccessInfo {
 
     private final CheckAccessInfoService checkAccessInfoService;
 
     @GetMapping("/accessinfo")
-    public AccessInfo checkAccessInfo(HttpServletRequest request) throws Exception  {
+    public String checkAccessInfo(HttpServletRequest request, Model model) throws Exception  {
 
-        AccessInfo accessInfo = AccessInfo.builder()
+        ktds.cloud.cloudkvtest.entity.AccessInfo accessInfo = ktds.cloud.cloudkvtest.entity.AccessInfo.builder()
             .ip(checkAccessInfoService.getIp(request))
             .os(checkAccessInfoService.getOs(request))
             .browser(checkAccessInfoService.getBrowser(request))
             .device(checkAccessInfoService.getDevice(request))
             .build();
 
-        return accessInfo;
+        model.addAttribute("accessInfo", accessInfo);
+
+        return "accessInfo";
     }
 
 }
